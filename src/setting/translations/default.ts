434ebe4e327aced001cfac32b7737e1132e0cb5a
avasaf <114611531+avasaf@users.noreply.github.com>
2025-09-06 13:00:52 +0200

    fix: render popup box shadow

--- src/setting/translations/default.ts
+++ src/setting/translations/default.ts
@@ -1,3 +1,8 @@
 export default {
-    videoUrl: 'Video Stream URL'
+  videoUrl: 'Video Feeds',
+  advanced: 'Advanced',
+  advancedStyling: 'Advanced Styling',
+  feedName: 'Feed Name',
+  feedUrl: 'Feed URL',
+  addFeed: 'Add Feed'
 }
