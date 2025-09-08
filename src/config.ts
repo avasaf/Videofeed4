bdae54b3279893454a725fb6ea2c133794f486ca
avasaf <114611531+avasaf@users.noreply.github.com>
2025-09-08 10:31:42 +0200

    fix popup overlay

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,40 @@
 import { type ImmutableObject } from 'jimu-core'
 
+export interface Feed {
+  name: string
+  url: string
+}
+
 export type IMConfig = ImmutableObject<{
-  videoUrl: string
+  feeds: Feed[]
   useAdvancedStyles: boolean
   widgetBackgroundColor: string
   widgetBorderColor: string
+  dropdownBackgroundColor: string
+  dropdownSectionBackgroundColor: string
+  dropdownSectionHoverBackgroundColor: string
+  dropdownBorderRadius: number
+  dropdownTextColor: string
+  dropdownSectionTextColor: string
+  dropdownSectionHoverTextColor: string
+  dropdownSectionHoverBorderRadius: number
+  dropdownArrowColor: string
+  expandButtonBackgroundColor: string
+  expandButtonIconColor: string
+  expandButtonBorderRadius: number
+  popupGap: number
+  popupPadding: number
+  popupItemPadding: number
+  popupBackgroundColor: string
+  popupBorderRadius: number
+  popupBlockPage: boolean
+  popupBlockPageColor: string
+  popupBoxShadowOffsetX: number
+  popupBoxShadowOffsetY: number
+  popupBoxShadowBlur: number
+  popupBoxShadowSpread: number
+  popupBoxShadowColor: string
+  markerBackgroundColor: string
+  markerTextColor: string
+  markerBorderRadius: number
 }>
